9c21ae12232fd029dc5d9e15d1580d4c3cd957e5
xuandav <164993335+xuandav@users.noreply.github.com>
2025-07-29 01:51:50 -0500

    Add includes/side_menu.php 1753771910054

---
+++ includes/side_menu.php
@@ -0,0 +1,74 @@
+<nav class="side-menu new-side-menu">
+    <ul class="side-menu-list">
+        <li class="side-menu-item active">
+            <a href="#" class="side-menu-link">
+                <span class="side-menu-icon">🏠</span>
+                <span class="side-menu-text">surco</span>
+            </a>
+        </li>
+        <li class="side-menu-item">
+            <a href="#" class="side-menu-link">
+                <span class="side-menu-icon">📡</span>
+                <span class="side-menu-text">Vivir</span>
+            </a>
+        </li>
+        <li class="side-menu-item">
+            <a href="#" class="side-menu-link">
+                <span class="side-menu-icon">▶️</span>
+                <span class="side-menu-text">pinza</span>
+            </a>
+        </li>
+        <li class="side-menu-item">
+            <a href="#" class="side-menu-link">
+                <span class="side-menu-icon">🔔</span>
+                <span class="side-menu-text">suscripción</span>
+            </a>
+        </li>
+    </ul>
+    <div class="side-menu-section">
+        <h4>Mi</h4>
+        <ul class="side-menu-list">
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">⏰</span>
+                    <span class="side-menu-text">Video visto recientemente</span>
+                </a>
+            </li>
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">❤️</span>
+                    <span class="side-menu-text">Me gusta el video</span>
+                </a>
+            </li>
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">📅</span>
+                    <span class="side-menu-text">Ver más tarde</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="side-menu-section">
+        <h4>Navegación</h4>
+        <ul class="side-menu-list">
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">🏆</span>
+                    <span class="side-menu-text">TOP100</span>
+                </a>
+            </li>
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">🔍</span>
+                    <span class="side-menu-text">Zoom</span>
+                </a>
+            </li>
+            <li class="side-menu-item">
+                <a href="#" class="side-menu-link">
+                    <span class="side-menu-icon">📰</span>
+                    <span class="side-menu-text">noticia</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
